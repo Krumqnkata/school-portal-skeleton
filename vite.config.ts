35fa5a47d6f4127e4fbe33092b7933ba58509df1
Krum <krumidanikrumov@abv.bg>
2025-11-26 22:02:42 +0200

    Merge branch 'main' of https://github.com/Krumqnkata/school-portal-skeleton

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ["a171186d71a1.ngrok-free.app"],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
