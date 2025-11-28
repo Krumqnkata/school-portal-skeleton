7d66a36b34be13e5c24a3572ceb08a70e7d4a122
Krum <krumidanikrumov@abv.bg>
2025-11-28 08:12:24 +0200

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -19,9 +19,8 @@ const Contact = () => {
                 <iframe
                   src="https://maps.google.com/maps?q=%D0%9F%D1%80%D0%BE%D1%84%D0%B5%D1%81%D0%B8%D0%BE%D0%BD%D0%B0%D0%BB%D0%BD%D0%B0%20%D0%B3%D0%B8%D0%BC%D0%BD%D0%B0%D0%B7%D0%B8%D1%8F%20%D0%BF%D0%BE%20%D0%BA%D0%BE%D0%BC%D0%BF%D1%8E%D1%82%D1%8A%D1%80%D0%BD%D0%B8%20%D0%BD%D0%B0%D1%83%D0%BA%D0%B8%20%D0%B8%20%D0%BC%D0%B0%D1%82%D0%B5%D0%BC%D0%B0%D1%82%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8%20%D0%B0%D0%BD%D0%B0%D0%BB%D0%B8%D0%B7%D0%B8%20%22%D0%9F%D1%80%D0%BE%D1%84.%20%D0%9C%D0%B8%D0%BD%D0%BA%D0%BE%20%D0%91%D0%B0%D0%BB%D0%BA%D0%B0%D0%BD%D1%81%D0%BA%D0%B8%22%2C%20%D0%BA%D0%B2.%20%D0%9E%D0%BF%D1%8A%D0%BB%D1%87%D0%B5%D0%BD%D1%81%D0%BA%D0%B8%2C%20%D1%83%D0%BB.%20%E2%80%9E%D0%A1%D1%82%D0%B5%D1%84%D0%B0%D0%BD%20%D0%A1%D0%BB%D0%B8%D0%B2%D0%BA%D0%BE%D0%B2%E2%80%9C%207%2C%206009%20%D0%A1%D1%82%D0%B0%D1%80%D0%B0%20%D0%97%D0%B0%D0%B3%D0%BE%D1%80%D0%B0&hl=bg&z=15&output=embed"
                   width="100%"
-                  height="300"
+                  height="450"
                   style={{ border: 0 }}
-                  allowFullScreen={false}
                   loading="lazy"
                   referrerPolicy="no-referrer-when-downgrade"
                   className="dark:invert-[90%] dark:hue-rotate-180"
